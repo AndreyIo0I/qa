aa06fe3a344a6f82300639422b7fbe9a544c4008
Andrey Arsbkv <andrbeckov@gmail.com>
2021-10-21 20:52:07 +0300

    2

--- lab2/main.js
+++ lab2/main.js
@@ -1,36 +1,78 @@
-const http = require('http')
 const fs = require('fs')
 const process = require('process')
-const url = process.argv[2]
+const urlApi = require('url')
 
-const URL_REGEXP = /href="([^"]+)"/g
+const URL_REGEXP = /(?:href|src|cite|formaction|icon|action|manifest|profile|background|longdesc|classid|codebase|data|poster)="([^"]+)"/g
+const parsedUrl = urlApi.parse(process.argv[2])
+const host = parsedUrl.host
+const urlBase = parsedUrl.protocol + '//' + host
 
-http.get(url, (res) => {
-	const {statusCode} = res
-	const contentType = res.headers['content-type']
+const parsedUrls = new Map()
 
-	let error
-	if (statusCode !== 200) {
-		error = new Error(`Request Failed. Status Code: ${statusCode}`)
-	} else if (contentType !== 'text/html') {
-		error = new Error(`Invalid content-type. Expected text/html but received ${contentType}`)
-	}
-	if (error) {
-		console.error(error.message)
-		res.resume()
-		return
-	}
+main()
 
-	res.setEncoding('utf8')
-	let rawData = ''
-	res.on('data', chunk => rawData += chunk)
-	res.on('end', () => {
-		let output = ''
-		const urlsArray = [...rawData.matchAll(URL_REGEXP)]
-		urlsArray.forEach(item => output += item[1] + '\n')
-		fs.writeFileSync('html.txt', rawData)
-		fs.writeFileSync('output.txt', output)
+function analyzeUrl(url) {
+	return new Promise(resolve => {
+		if (parsedUrls.has(url)) {
+			return
+		}
+		const http = url.startsWith('https') ? require('https') : require('http')
+		http.get(url, (res) => {
+			const {statusCode} = res
+			const contentType = res.headers['content-type']
+
+			parsedUrls.set(url, statusCode)
+
+			if (!(statusCode === 200 && contentType && contentType.includes('text/html'))) {
+				res.resume()
+				--f
+				return
+			}
+
+			res.setEncoding('utf8')
+			let rawData = ''
+			res.on('data', chunk => rawData += chunk)
+			res.on('end', () => {
+				const urlsArray = [...rawData.matchAll(URL_REGEXP)]
+				urlsArray.forEach(item => {
+					item = item[1]
+					if (item.startsWith('http://') || item.startsWith('https://')) {
+						analyzeUrl(item)
+					}
+					else if (item[0] === '/') {
+						analyzeUrl(urlBase + '/' + item)
+					}
+					else {
+						analyzeUrl(urlBase + '//' + item)
+					}
+				})
+			})
+		})
+		.on('error', () => {})
 	})
-}).on('error', e => {
-	console.error(`Got error: ${e.message}`)
-})
+}
+
+async function main() {
+	await analyzeUrl(process.argv[2])
+
+	let ok = ''
+	let okCount = 0
+	let bad = ''
+	let badCount = 0
+
+	parsedUrls.forEach((status, url) => {
+		if ([200].includes(status)) {
+			ok += status + ' ' + url + '\n'
+			++okCount
+		}
+		else {
+			bad += status + ' ' + url + '\n'
+			++badCount
+		}
+	})
+
+	ok += `${okCount}\n${new Date(Date.now()).toLocaleString('ru-RU')}\n`
+	bad += `${badCount}\n${new Date(Date.now()).toLocaleString('ru-RU')}\n`
+	fs.writeFileSync('ok.txt', ok)
+	fs.writeFileSync('bad.txt', bad)
+}
