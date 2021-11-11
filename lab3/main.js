4781a6afbac499a03865b3f057edd39633c68e5a
Andrey Arsbkv <andrbeckov@gmail.com>
2021-11-11 09:10:04 +0300

    3 лаба

---
+++ lab3/main.js
@@ -0,0 +1,74 @@
+export class Rectangle {
+	#x
+	#y
+	#height
+	#width
+
+	constructor(x, y, height, width) {
+		this.setX(x)
+		this.setY(y)
+		this.setHeight(height)
+		this.setWidth(width)
+	}
+
+	setX(x) {
+		if (typeof x !== 'number') {
+			throw Error('invalid x')
+		}
+		this.#x = x
+	}
+
+	setY(y) {
+		if (typeof y !== 'number') {
+			throw Error('invalid y')
+		}
+		this.#y = y
+	}
+
+	setHeight(height) {
+		if (typeof height !== 'number' || height <= 0) {
+			throw Error('invalid height')
+		}
+		this.#height = height
+	}
+
+	setWidth(width) {
+		if (typeof width !== 'number' || width <= 0) {
+			throw Error('invalid width')
+		}
+		this.#width = width
+	}
+
+	getX() {
+		return this.#x
+	}
+
+	getY() {
+		return this.#y
+	}
+
+	getWidth() {
+		return this.#width
+	}
+
+	getHeight() {
+		return this.#height
+	}
+
+	getArea() {
+		return this.#height * this.#width
+	}
+
+	getPerimeter() {
+		return 2 * (this.#height + this.#width)
+	}
+
+	intersect(rect) {
+		if (!(rect instanceof Rectangle)) {
+			throw Error('invalid rect')
+		}
+		const xIntersection = this.#x + this.#width > rect.getX() && rect.getX() + rect.getWidth() > this.getX()
+		const yIntersection = this.#y + this.#height > rect.getY() && rect.getY() + rect.getHeight() > this.getY()
+		return xIntersection && yIntersection
+	}
+}
---
+++ lab3/main.test.js
@@ -0,0 +1,60 @@
+import {Rectangle} from './main.js'
+
+describe('test rectangle', () => {
+	test('invalid x', () => {
+		expect(() => {
+			const rect = new Rectangle('lalala', 2, 3, 4)
+		}).toThrowError('invalid x')
+	})
+	test('invalid y', () => {
+		expect(() => {
+			const rect = new Rectangle(1, true, 3, 4)
+		}).toThrowError('invalid y')
+	})
+	test('invalid height', () => {
+		expect(() => {
+			const rect = new Rectangle(1, 2, -3000, 4)
+		}).toThrowError('invalid height')
+	})
+	test('invalid width', () => {
+		expect(() => {
+			const rect = new Rectangle(1, 2, 3000, 4)
+			rect.setWidth('f')
+		}).toThrowError('invalid width')
+	})
+	test('invalid rect', () => {
+		expect(() => {
+			const rect = new Rectangle(1, 2, 3000, 4)
+			rect.intersect('123')
+		}).toThrowError('invalid rect')
+	})
+	test('get area', () => {
+		const rect = new Rectangle(1, 2, 3, 4)
+		expect(rect.getArea()).toBe(12)
+	})
+	test('get perimeter', () => {
+		const rect = new Rectangle(1, 2, 3, 4)
+		expect(rect.getPerimeter()).toBe(14)
+	})
+	test('get x', () => {
+		const rect = new Rectangle(1, 2, 3, 4)
+		expect(rect.getX()).toBe(1)
+	})
+	test('get y', () => {
+		const rect = new Rectangle(1, 2, 3, 4)
+		expect(rect.getY()).toBe(2)
+	})
+	test('get height', () => {
+		const rect = new Rectangle(1, 2, 3, 4)
+		expect(rect.getHeight()).toBe(3)
+	})
+	test('get width', () => {
+		const rect = new Rectangle(1, 2, 3, 4)
+		expect(rect.getWidth()).toBe(4)
+	})
+	test('intersection', () => {
+		const rect1 = new Rectangle(1, 2, 3, 4)
+		const rect2 = new Rectangle(1, 2, 3, 4)
+		expect(rect1.intersect(rect2)).toBe(true)
+	})
+})
